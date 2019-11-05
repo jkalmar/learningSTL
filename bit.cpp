72289ad6967b37cc13fcd6a593e085266066cdc1
jkalmar <janko.kalmar@gmail.com>
2019-11-05 22:44:10 +0100

    [Numeric][bit] Example for all functions expect of std::bit_cast

--- bit.cpp
+++ bit.cpp
@@ -7,6 +7,9 @@ void bit_cast_example()
 
 }
 
+/**
+ * Checks if x is an integral power of two
+ */
 void ispow2_example()
 {
     std::cout << "1 ispow2: " << std::ispow2( 1u ) << std::endl;
@@ -34,8 +37,6 @@ void log2p1_example()
 {
     uint64_t number = 0b111100001; // 9 bits
 
-    __uint128_t big_number = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-
     std::cout << number << " log2p1: " <<  std::log2p1( number ) << std::endl;
 
 }
@@ -53,32 +54,48 @@ void rotl_example()
 
 void rotr_example()
 {
-
+    uint8_t i = 0b00011101;
+    std::cout << "i          = " << std::bitset<8>(i) << '\n';
+    std::cout << "rotr(i,0)  = " << std::bitset<8>(std::rotr(i,0)) << '\n';
+    std::cout << "rotr(i,1)  = " << std::bitset<8>(std::rotr(i,1)) << '\n';
+    std::cout << "rotr(i,4)  = " << std::bitset<8>(std::rotr(i,4)) << '\n';
+    std::cout << "rotr(i,9)  = " << std::bitset<8>(std::rotr(i,9)) << '\n';
+    std::cout << "rotr(i,-1) = " << std::bitset<8>(std::rotr(i,-1)) << '\n';
 }
 
 void countl_zero_example()
 {
+    uint8_t i = 0b00011101;
 
+    std::cout << "Left zeros in: " << std::bitset< 8 >( i ).to_string() << " " << std::countl_zero( i ) << std::endl;
 }
 
 void countl_one_example()
 {
+    uint8_t i = 0b00011101;
 
+    std::cout << "Left ones in: " << std::bitset< 8 >( i ).to_string() << " " << std::countl_one( i ) << std::endl;
 }
 
 void countr_zero_example()
 {
+    uint8_t i = 0b00011101;
 
+    std::cout << "Right zeros in: " << std::bitset< 8 >( i ).to_string() << " " << std::countr_zero( i ) << std::endl;
 }
 
 void countr_one_example()
 {
+    uint8_t i = 0b00011101;
 
+    std::cout << "Right ones in: " << std::bitset< 8 >( i ).to_string() << " " << std::countr_one( i ) << std::endl;
 }
 
 void popcount_example()
 {
+    uint8_t i = 0b00011101;
 
+    std::cout << "All ones in: " << std::bitset< 8 >( i ).to_string() << " " << std::popcount( i ) << std::endl;
 }
 
 void endian_example()
@@ -97,34 +114,37 @@ void endian_example()
     }
 }
 
- 
+
 int main()
 {
     endian_example();
-    rotl_example();
     ceil2_example();
     floor2_example();
     ispow2_example();
     log2p1_example();
-
-
-
+    rotl_example();
+    rotr_example();
+    countl_zero_example();
+    countl_one_example();
+    countr_zero_example();
+    countr_one_example();
+    popcount_example();
 }
 
 /**
 TODO: things to cover:
-    - bit_cast
-    - ispow2
-    - ceil2
-    - floor2
-    - log2p1
-    - rotl      ... OK
-    - rotr
-    - countl_zero
-    - countl_one
-    - countr_zero
-    - countr_one
-    - popcount
-    - endian    ... OK
+    - bit_cast      ... PART2
+    - ispow2        ... OK
+    - ceil2         ... OK
+    - floor2        ... OK
+    - log2p1        ... OK
+    - rotl          ... OK
+    - rotr          ... OK
+    - countl_zero   ... OK
+    - countl_one    ... OK
+    - countr_zero   ... OK
+    - countr_one    ... OK
+    - popcount      ... OK
+    - endian        ... OK
 */
 
