2a623a0eaff94fd3a205f683c29d4b659a7dfd68
jkalmar <janko.kalmar@gmail.com>
2019-11-10 14:16:42 +0100

    Use namespace std to save space and make code more readable

--- bit.cpp
+++ bit.cpp
@@ -2,19 +2,21 @@
 #include <bitset>
 #include <iostream>
 
+using namespace std;
+
 void endian_example()
 {
-    if(std::endian::native == std::endian::big)
+    if(endian::native == endian::big)
     {
-        std::cout << "This machine uses BIG endian" << std::endl;
+        cout << "This machine uses BIG endian" << endl;
     }
-    else if(std::endian::native == std::endian::little)
+    else if(endian::native == endian::little)
     {
-        std::cout << "This machine uses little endian" << std::endl;
+        cout << "This machine uses little endian" << endl;
     }
     else
     {
-        std::cout << "This machine endian is unknown" << std::endl;
+        cout << "This machine endian is unknown" << endl;
     }
 }
 
@@ -24,59 +26,59 @@ void bit_examples()
 
     uint16_t number = 0x1D10u; // 0b0001110100010000u;
 
-    std::cout << "Printing bits in number is easy using std::bitset" << std::endl;
-    std::cout << number << " as bitstring: " << std::bitset< 16 >( number ) << std::endl;
-    std::cout << std::endl;
+    cout << "Printing bits in number is easy using bitset" << endl;
+    cout << number << " as bitstring: " << bitset< 16 >( number ) << endl;
+    cout << endl;
 
-    std::cout << "Counting bits" << std::endl;
-    std::cout << "how many actuall bits we need to represent the number?" << std::endl;
-    std::cout << number << " log2p1:       " <<  std::log2p1( number ) << std::endl;
-    std::cout << std::endl;
+    cout << "Counting bits" << endl;
+    cout << "how many actuall bits we need to represent the number?" << endl;
+    cout << number << " log2p1:       " <<  log2p1( number ) << endl;
+    cout << endl;
 
-    std::cout << "how many 0, or 1 bits is in the number from left or right" << std::endl;
-    std::cout << "To count bits from left use std::countl_zero or std::countl_one" << std::endl;
-    std::cout << "Left zeros in:  " << std::bitset< 16 >( number ) << " " << std::countl_zero( number ) << std::endl;
-    std::cout << "Left ones in:   " << std::bitset< 16 >( number ) << " " << std::countl_one( number ) << std::endl;
+    cout << "how many 0, or 1 bits is in the number from left or right" << endl;
+    cout << "To count bits from left use countl_zero or countl_one" << endl;
+    cout << "Left zeros in:  " << bitset< 16 >( number ) << " " << countl_zero( number ) << endl;
+    cout << "Left ones in:   " << bitset< 16 >( number ) << " " << countl_one( number ) << endl;
 
-    std::cout << "To count bits from right use std::countr_zero or std::countr_one" << std::endl;
-    std::cout << "Right zeros in: " << std::bitset< 16 >( number ) << " " << std::countr_zero( number ) << std::endl;
-    std::cout << "Right ones in:  " << std::bitset< 16 >( number ) << " " << std::countr_one( number ) << std::endl;
+    cout << "To count bits from right use countr_zero or countr_one" << endl;
+    cout << "Right zeros in: " << bitset< 16 >( number ) << " " << countr_zero( number ) << endl;
+    cout << "Right ones in:  " << bitset< 16 >( number ) << " " << countr_one( number ) << endl;
 
-    std::cout << "how many 1s in total are in number" << std::endl;
-    std::cout << "All ones in:    " << std::bitset< 16 >( number ) << " " << std::popcount( number ) << std::endl;
-    std::cout << std::endl;
+    cout << "how many 1s in total are in number" << endl;
+    cout << "All ones in:    " << bitset< 16 >( number ) << " " << popcount( number ) << endl;
+    cout << endl;
 
-    std::cout << "To do some power operations we can use:" << std::endl;
-    std::cout << " - std::ispow2" << std::endl;
-    std::cout << " - std::floor2" << std::endl;
-    std::cout << " - std::ceil2" << std::endl;
+    cout << "To do some power operations we can use:" << endl;
+    cout << " - ispow2" << endl;
+    cout << " - floor2" << endl;
+    cout << " - ceil2" << endl;
 
-    uint16_t floor = std::floor2( number ); // find nearest integral power of two that is <= than number
-    uint16_t ceil = std::ceil2( number );   // find nearest integral power of two that is >= than number
+    uint16_t floor = floor2( number ); // find nearest integral power of two that is <= than number
+    uint16_t ceil = ceil2( number );   // find nearest integral power of two that is >= than number
 
-    std::cout << number << " ispow2: " << std::boolalpha << std::ispow2( number ) << std::endl;
-    std::cout << number << " floor2: " <<  std::floor2( number ) << std::endl;
-    std::cout << floor << " floor2: " <<  std::floor2( floor ) << std::endl;
-    std::cout << number << " ceil2:  " <<  std::ceil2( number ) << std::endl;
-    std::cout << ceil << " ceil2:  " <<  std::ceil2( ceil ) << std::endl;
-    std::cout << floor << " ispow2: " << std::boolalpha << std::ispow2( floor ) << std::endl;
-    std::cout << ceil << " ispow2: " << std::boolalpha << std::ispow2( ceil ) << std::endl;
+    cout << number << " ispow2: " << boolalpha << ispow2( number ) << endl;
+    cout << number << " floor2: " <<  floor2( number ) << endl;
+    cout << floor << " floor2: " <<  floor2( floor ) << endl;
+    cout << number << " ceil2:  " <<  ceil2( number ) << endl;
+    cout << ceil << " ceil2:  " <<  ceil2( ceil ) << endl;
+    cout << floor << " ispow2: " << boolalpha << ispow2( floor ) << endl;
+    cout << ceil << " ispow2: " << boolalpha << ispow2( ceil ) << endl;
 
-    std::cout << "Lets check bits just to give you an idea what is happening" << std::endl;
-    std::cout << "number in binary: "  << std::bitset< 16 >( number ) << std::endl;
-    std::cout << "floor in binary:  "  << std::bitset< 16 >( floor ) << std::endl;
-    std::cout << "ceil in binary:   "  << std::bitset< 16 >( ceil ) << std::endl;
-    std::cout << std::endl;
+    cout << "Lets check bits just to give you an idea what is happening" << endl;
+    cout << "number in binary: "  << bitset< 16 >( number ) << endl;
+    cout << "floor in binary:  "  << bitset< 16 >( floor ) << endl;
+    cout << "ceil in binary:   "  << bitset< 16 >( ceil ) << endl;
+    cout << endl;
 
-    std::cout << "For shifting bits c++ has some options" << std::endl;
-    std::cout << "  - <<, >> - bitwise left/right shift" << std::endl;
-    std::cout << "  - std::rotl, std::rotr - bitwise left/right rotarion" << std::endl;
+    cout << "For shifting bits c++ has some options" << endl;
+    cout << "  - <<, >> - bitwise left/right shift" << endl;
+    cout << "  - rotl, rotr - bitwise left/right rotarion" << endl;
 
-    std::cout << "number in binary: " << std::bitset< 16 >( number ) << std::endl;
-    std::cout << "rotl(number, 8):  " << std::bitset< 16 >( std::rotl( number, 8 ) ) << std::endl;
-    std::cout << "number << 8:      " << std::bitset< 16 >( number << 8 ) << std::endl;
-    std::cout << "rotr(number, 8):  " << std::bitset< 16 >( std::rotr( number, 8 ) ) << std::endl;
-    std::cout << "number >> 8:      " << std::bitset< 16 >( number >> 8 ) << std::endl;
+    cout << "number in binary: " << bitset< 16 >( number ) << endl;
+    cout << "rotl(number, 8):  " << bitset< 16 >( rotl( number, 8 ) ) << endl;
+    cout << "number << 8:      " << bitset< 16 >( number << 8 ) << endl;
+    cout << "rotr(number, 8):  " << bitset< 16 >( rotr( number, 8 ) ) << endl;
+    cout << "number >> 8:      " << bitset< 16 >( number >> 8 ) << endl;
 }
 
 /**
